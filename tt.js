85e6457d980f3aa82d510170ef8a0ec160460fec
Unknown <RPowell777@west-mec.org>
2018-02-01 15:26:21 -0700

    Started working with variables array
    Useed variables to define the days of the week, teams that they will play, and if the team is going to be home or away.

---
+++ tt.js
@@ -0,0 +1,19 @@
+/*
+JavaScript
+Exercise 3
+
+Tipton Turbines
+Calendar web page
+Author: Ryan Powell
+Date: 2.1.18
+
+Filename: tt.js
+*/
+
+// global variables
+
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var opponents = ["Lightning", "Combines", "Combines", "Lightning", "Lightning", "Lightning", "Lightning", "Barn Raisers", "Barn Raisers", "Barn Raisers", "Sodbusters", "Sodbusters", "Sodbusters", "Sodbusters", "(off)", "River Riders", "River Riders", "River Riders", "Big Dippers", "Big Dippers", "Big Dippers", "(off)", "Sodbusters", "Sodbusters", "Sodbusters", "Combines", "Combines", "Combines", "(off)", "(off)"];
+
+var gameLocation = ["away", "away", "away", "away", "home", "home", "home", "home", "home", "home", "home", "away", "away", "away", "away", "", "away", "away", "away", "away", "away", "away", "", "home", "home", "home", "home", "home", "home", "", ""];
